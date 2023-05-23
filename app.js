f2b7db2b34e9560322d5d7f14dd7574a615f6739
José Lázaro <contato.jlazaro@gmail.com>
2023-05-23 20:53:53 -0300

    create list of categories

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose').default
 const admin = require('./routes/admin')
 const path = require('path')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const app = express()
 
 //Config
+
+    //Session
+        app.use(session({
+            secret: '123',
+            resave: true,
+            saveUninitialized: true,
+        }))
+
+        app.use(flash())
+    //Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next()
+        })
     //body Parser
         app.use(express.urlencoded({ extended: true}))
         app.use(express.json())
@@ -27,6 +44,11 @@ const app = express()
 
     //Public
         app.use(express.static(path.join(__dirname, 'public')))
+
+        app.use((req, res, next) => {
+            console.log('Middleware')
+            next()
+        })
 
 //Routes
     app.use('/admin', admin)
--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,12 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render('admin/categorias')
+    Categoria.find().lean().then((categorias) => {
+        res.render('admin/categorias', {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao listar as categorias')
+        res.redirect('/admin')
+    })
 })
 
 router.get('/categorias/add', (req, res) => {
@@ -21,15 +26,36 @@ router.get('/categorias/add', (req, res) => {
 })
 
 router.post("/categorias/nova", (req, res) => {
-    const novaCategoria = ({
-        nome: req.body.nome,
-        slug: req.body.slug,
-    })
-    new Categoria(novaCategoria).save().then(() => {
-        console.log('Categoria criada com sucesso')
-    }).catch((err) => {
-        console.log('Erro ao salvar categoria', err)
-    })
+
+    const erros = []
+
+    if (!req.body.nome){
+        erros.push({texto: 'Nome inválido'})
+    }
+
+    if (!req.body.slug) {
+        erros.push({texto: 'Slug inválido'})
+    }
+
+    if (req.body.nome.length < 2) {
+        erros.push({texto: 'Nome da categoria muito pequeno'})
+    }
+
+    if (erros.length > 0) {
+        res.render('admin/addcategorias', {erros: erros})
+    } else {
+        const novaCategoria = ({
+            nome: req.body.nome,
+            slug: req.body.slug,
+        })
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso!')
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash('error_msg', 'Erro ao salvar categoria.')
+            res.redirect('/admin')
+        })
+    }
 })
 
 module.exports = router
